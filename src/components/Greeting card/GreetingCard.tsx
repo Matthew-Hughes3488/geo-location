7a38fd6d4b3c0df1d00bad87da28b6536aa944ab
Matthew <12hughesm@gmail.com>
2024-02-29 20:42:59 +0000

    Added HTML and props to the greeting card

---
+++ src/components/Greeting card/GreetingCard.tsx
@@ -0,0 +1,12 @@
+type GreetingCardProps = {
+    timeOfDay : "Morning" | "Evening" | "Night"
+}
+
+export const GreetingCard = ({timeOfDay} : GreetingCardProps) => {
+  return (
+    <section className='greeting-card'>
+        <h2 className='greeting-card__title'>Good {timeOfDay} Wizard</h2>
+        <h3 className='greeting-card__text'>Here is everything you need to know to have a magical day</h3>
+    </section>
+  )
+}
