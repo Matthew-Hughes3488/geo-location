a8e2561629302a3cccda292e9c933cd4bf05289b
Matthew <12hughesm@gmail.com>
2024-03-01 02:41:04 +0000

    reformat code

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -1,50 +1,42 @@
-import "./Main.scss"
+import "./Main.scss";
 import { GreetingCard } from "../../components/Greeting card/GreetingCard";
 import { useEffect, useState } from "react";
 
-const getGreeting = (currentHour : number) : String =>{
-    if(currentHour < 12)
-        return "Morning";
-    else if(currentHour < 18)
-        return "Afternoon";
-    else if(currentHour < 21)
-        return "Evening";
-    else
-        return "Night"
-}
+const getGreeting = (currentHour: number): String => {
+  if (currentHour < 12) return "Morning";
+  else if (currentHour < 18) return "Afternoon";
+  else if (currentHour < 21) return "Evening";
+  else return "Night";
+};
 
 export const Main = () => {
+  const [location, setLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  } | null>(null);
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
 
-    const [location, setLocation] = useState<{ latitude: number, longitude: number } | null>(null);
-    const currentTime = new Date();
-    const currentHour = currentTime.getHours();
-
-    useEffect(() => {
-        const getLocation = () => {
-          if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(
-              (position) => {
-                const { latitude, longitude } = position.coords;
-                setLocation({ latitude, longitude });
-              },
-              (error) => {
-                console.error("Error getting geolocation:", error);
-              }
-            );
-          } else {
-            console.log("Geolocation is not supported");
-          }
-        };
-    
-        getLocation();
-    
-        return () => {
-        };
-      }, []);
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setLocation({ latitude, longitude });
+        },
+        (error) => {
+          console.error("Error getting geolocation:", error);
+        }
+      );
+    } else {
+      console.log("Geolocation is not supported");
+    }
+    return () => {};
+  }, []);
 
   return (
     <main className="main-content">
-        <GreetingCard timeOfDay={getGreeting(currentHour)}/>
+      <GreetingCard timeOfDay={getGreeting(currentHour)} />
     </main>
-  )
-}
+  );
+};
