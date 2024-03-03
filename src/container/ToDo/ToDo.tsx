bbef7bbef9dae2f4b0a6a2c6f008dc222005f73a
Matthew <12hughesm@gmail.com>
2024-03-03 18:29:50 +0000

    created test data

--- src/container/ToDo/ToDo.tsx
+++ src/container/ToDo/ToDo.tsx
@@ -1,14 +1,7 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Task } from "../../types/Task";
 
 export const ToDo = () => {
     
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [taskInput, setTaskInput] = useState("");
-  const [numberOfTasksAdded, setNumberOfTasksAdded] = useState(0); // Used as a id and key for each task item component created
-
-  
-
-
   return <></>;
 };
